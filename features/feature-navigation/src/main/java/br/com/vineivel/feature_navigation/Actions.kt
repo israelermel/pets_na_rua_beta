40ace9365b1984f91888ba5844149da66b166f37
israel ermel <israelermel@gmail.com>
2020-04-04 16:20:22 -0300

    adicionado feature module navigation

---
+++ features/feature-navigation/src/main/java/br/com/vineivel/feature_navigation/Actions.kt
@@ -0,0 +1,12 @@
+package br.com.vineivel.feature_navigation
+
+import android.content.Context
+import android.content.Intent
+
+object Actions {
+    fun openLoginIntent(context: Context) =
+        internalIntent(context, "action.login.open")
+
+    private fun internalIntent(context: Context, action: String) =
+        Intent(action).setPackage(context.packageName)
+}
