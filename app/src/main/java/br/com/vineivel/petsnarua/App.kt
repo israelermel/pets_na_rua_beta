d7f7202f2a3c94cdff3e6f00043394b6d95e4fb4
israel ermel <israelermel@gmail.com>
2020-06-24 19:47:01 -0300

    configuracao AppCenter

--- app/src/main/java/br/com/vineivel/petsnarua/App.kt
+++ app/src/main/java/br/com/vineivel/petsnarua/App.kt
@@ -4,15 +4,30 @@ import android.app.Application
 import br.com.vineivel.data.di.dataModule
 import br.com.vineivel.emailregister.di.loginModule
 import br.com.vineivel.googleregister.di.googleRegisterModule
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         initDependencyInjection()
+
+        AppCenter.start(
+            this,
+            "2fa1e53b-6719-4448-8db1-0e10e6630986",
+            Analytics::class.java, Crashes::class.java, Distribute::class.java
+        )
+
+        Distribute.setEnabled(true)
+        Analytics.setEnabled(true)
+        Crashes.setEnabled(true)
     }
 
     private fun initDependencyInjection() {
